595c5c6b42a7bb1e18e29dddfdce5a0ec89aeb5d
Subhash Sharma <subhash@modelogiq.com>
2016-03-15 14:17:38 +0530

    wrote a small compiler

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
-///<reference path="" />
+///<reference path="./typings/main.d.ts" />
 
 import * as ts from "typescript";
 
-function compile(filenames: []string, options: ts.CompilerOptions): void {
+function compile(filenames: Array<string>, options: ts.CompilerOptions): void {
+	let program = ts.createProgram(filenames, options);
+	let emitResult = program.emit();
+	let allDiags = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+	allDiags.forEach(diagnostic => {
+		let {line, character} = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
+		let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+		console.log(`${diagnostic.file.fileName} (${line + 1}, ${character + 1}): ${message}`);
+	});
 
+	let exitCode = emitResult.emitSkipped ? 1 : 0;
+	console.log(`Process exiting with code '${exitCode}'.`);
+	process.exit(exitCode);
 }
+
+compile(process.argv.slice(2), {
+	noEmitOnError: true,
+	noImplicitAny: true,
+	target: ts.ScriptTarget.ES5,
+	module: ts.ModuleKind.CommonJS
+});
